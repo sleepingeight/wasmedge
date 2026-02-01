42dc397e8b9b43ad862affbd5c013942e2ab589c
sleepingieght <i-mamidi.teja@devrev.ai>
2026-02-01 16:52:04 +0530

    remove unwanted files

--- .commitlintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    // Set to warning level since Dependabot creates long commit message lines
-    // that we have no control.
-    "header-max-length": [1, "always", 100],
-    "body-max-line-length": [1, "always", 100],
-  },
-};
